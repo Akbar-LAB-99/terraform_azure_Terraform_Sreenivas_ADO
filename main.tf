48f4a9d3a7202613d4f1dd68e2eb7ef1fad027c7
ksreenivasmithileswar <41468694+ksreenivasmithileswar@users.noreply.github.com>
2025-04-05 23:54:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -103,10 +103,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
+   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20_04-lts"
+    sku       = "20_04-lts-gen2"
     version   = "latest"
   }
 }
