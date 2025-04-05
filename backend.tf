d2a0ba6dda18263a3246f99ad589db09392ff585
ksreenivasmithileswar <41468694+ksreenivasmithileswar@users.noreply.github.com>
2025-04-05 21:49:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "crif"
-    storage_account_name = "terraformcrif"
+    storage_account_name = "terraformcrif1"
     container_name       = "tfstatecrif"
     key                  = "default.terraform.tfstate"
   }
