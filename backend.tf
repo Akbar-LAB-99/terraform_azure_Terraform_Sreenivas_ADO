70700eb6f6398280bea371edd30c52960cba786b
Akbar335 <62642055+Akbar335@users.noreply.github.com>
2025-04-27 12:27:55 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "crif"
-    storage_account_name = "terraformcrif2"
-    container_name       = "tfstatecrif"
+    resource_group_name  = "demo_rg"
+    storage_account_name = "demo_storageacc"
+    container_name       = "demo_CN"
     key                  = "default.terraform.tfstate"
   }
 }
